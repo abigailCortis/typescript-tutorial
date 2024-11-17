70158e3716f5a658e310e05f71de78d9721f9e6f
Abigail Cortis <cortisbiggirl@gmail.com>
2024-11-17 22:27:48 +0100

    while loop

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
-for(let i=1; i<=10; i++){
-    if (i % 2 ==0) {
+let i: number = 1;
+
+while (i<= 10) {
+    if (i % 2 == 0) {
         console.log(`${i} - even`);
     } else {
         console.log(`${i} - odd`);
     }
+
+    i++;
 }
