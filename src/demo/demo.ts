35488aa1837ee2471fccad75af70128bace0d964
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-08 22:30:54 +0100

    creating and using promises

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,5 +1,30 @@
-import { Movie, Logger, CastMember as Actor } from './interfaces';
-import { Performer, Documentary } from './classes';
-import * as Utility from './functions';
+import { Movie, Logger, CastMember as Actor } from './demo/interfaces';
+import { Performer, Documentary } from './demo/classes';
+import * as Utility from './demo/functions';
 
-let allMovies: Movie[] = Utility.GetAllMovies();
+function getMoviesByDirector(director: string): Promise<string[]> {
+    let p: Promise<string[]> = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let foundMovies: string[] = Utility.GetTitles(director);
+
+            if(foundMovies.length > 0) {
+                resolve(foundMovies);
+            } else {
+                reject('No movies found for that director.');
+            }
+        }, 2000);
+    });
+
+    return p;
+}
+
+console.log('Beginning search ...');
+getMoviesByDirector('George Lucas')
+    .then(titles => {
+        console.log(`Found titles: ${titles}`);
+        throw 'something bad happened';
+        return titles.length;
+    }, reason => {return 0;})
+    .then(numOfMovies => console.log(`Number of movies found: ${numOfMovies}`))
+    .catch(reason => console.log(`Error: ${reason}`));
+console.log('Search submitted ... ');
