78578a0d975dc2b9691c8b52afe4f1922089998e
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-08 17:30:02 +0100

    demo to use modules

---
+++ src/module_7_organising_code_in_modules/module_resolution.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+
+// relative imports
+import { Laptop } from '/hardware';
+import { Developer } from './person';
+import { NewHire } from '../HR/recruiting';
+
+// non-relative imports
+import * as $ from 'jquery';
+import * as lodash from 'lodash';
