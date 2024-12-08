76bca2a9914b3a0982cc8b2f474e736f9bf48813
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-08 17:10:16 +0100

    exporting and importing

---
+++ src/module_7_organising_code_in_modules/exporting_and_importing.ts
@@ -0,0 +1,23 @@
+// person.ts
+export interface Person { }
+
+export function hireDeveloper(): void { }
+
+export default class Employee { }
+
+class Manager { } // not accessible outside the module
+
+export { Person, hireDeveloper, Employee as StaffMember};
+
+// player.ts
+import {Person, hireDeveloper} from './person';
+let human: Person;
+
+import Worker from './person';
+let engineer: Worker = new Worker();
+
+import { StaffMember as CoWorker} from './person';
+let emp: CoWorker = new CoWorker();
+
+import * as HR from './person';
+HR.hireDeveloper();
